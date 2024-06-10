7c77c617071bcca4c4ecd549907b440cf9b4ab24
bodadeshailendra <72300433+bodadeshailendra@users.noreply.github.com>
2024-06-10 14:22:19 +0530

    Add files via upload

---
+++ ApiTestGeneratorController-v1.java
@@ -0,0 +1,202 @@
+/**
+ * 
+ */
+package com.ai.testgenerator.controller;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ai.testgenerator.service.AutoTestGeneratorService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * 
+ */
+@RestController
+@CrossOrigin
+@OpenAPIDefinition
+//@RequestMapping("/v1")
+public class ApiTestGeneratorController {
+
+	@Autowired
+	AutoTestGeneratorService autoTestGeneratorService;
+
+	@PostMapping(name = "CreatePostmanCollection", value = "/createPostmanCollection", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Resource> createPostmanCollection(
+			@RequestPart(required = true, name = "apiSpec") @NotNull MultipartFile apiSpec,
+			@RequestPart(name = "apiData") MultipartFile dataFile) throws Exception {
+		File file = autoTestGeneratorService.createPostmanCollection(apiSpec, dataFile);
+		Path path = Paths.get(file.getAbsolutePath());
+		ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+		HttpHeaders header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+		header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		header.add("Pragma", "no-cache");
+		header.add("Expires", "0");
+
+		return ResponseEntity.ok().headers(header).contentLength(file.length())
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
+	}
+
+	@PostMapping(name = "CreatePostmanCollectionWithDataMap", value = "/createPostmanCollectionWithDataMap", consumes = {
+			MediaType.MULTIPART_FORM_DATA_VALUE })
+	public ResponseEntity<Resource> createPostmanCollection(
+			@RequestPart(required = true, name = "apiSpec") @NotNull MultipartFile apiSpec,
+			@RequestPart(name = "apiData") @NotNull HashMap<String, Object> dataMap) throws Exception {
+
+		File file = autoTestGeneratorService.createPostmanCollectionWithDataMap(apiSpec, dataMap);
+		Path path = Paths.get(file.getAbsolutePath());
+		ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+		HttpHeaders header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+		header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		header.add("Pragma", "no-cache");
+		header.add("Expires", "0");
+
+		return ResponseEntity.ok().headers(header).contentLength(file.length())
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
+	}
+
+	@PostMapping(name = "CreatePostmanCollectionWithDataMapAndFiles", value = "/createPostmanCollectionWithDataMapAndFiles", consumes = {
+			MediaType.MULTIPART_FORM_DATA_VALUE })
+	public ResponseEntity<Resource> createPostmanCollection(
+			@RequestPart(required = true, name = "apiSpec") @NotNull MultipartFile apiSpec,
+			@RequestPart(name = "apiData") @NotNull HashMap<String, Object> dataMap,
+			@RequestPart(name = "files") @Nullable List<MultipartFile> datafiles) throws Exception {
+
+		File file;
+		if (datafiles != null && !datafiles.isEmpty()) {
+			file = autoTestGeneratorService.createPostmanCollectionWithDataMap(apiSpec, dataMap, datafiles);
+		} else {
+			file = autoTestGeneratorService.createPostmanCollectionWithDataMap(apiSpec, dataMap);
+		}
+
+		Path path = Paths.get(file.getAbsolutePath());
+		ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+		HttpHeaders header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+		header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		header.add("Pragma", "no-cache");
+		header.add("Expires", "0");
+
+		return ResponseEntity.ok().headers(header).contentLength(file.length())
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
+	}
+
+	@PostMapping(name = "RunPostmanCollection", value = "/runPostmanCollection", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Resource> runPostmanCollection(
+			@RequestPart(required = true, name = "postmanCollection") @NotNull MultipartFile postmanCollection)
+			throws Exception {
+		File file = autoTestGeneratorService.runPostmanCollection(postmanCollection);
+		Path path = Paths.get(file.getAbsolutePath());
+		ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+		HttpHeaders header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+		header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		header.add("Pragma", "no-cache");
+		header.add("Expires", "0");
+
+		return ResponseEntity.ok().headers(header).contentLength(file.length())
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
+	}
+
+	@PostMapping(name = "RunPostmanCollectionWithDataFile", value = "/runPostmanCollectionWithDataFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Resource> runPostmanCollection(
+			@RequestPart(required = true, name = "postmanCollection") @NotNull MultipartFile postmanCollection,
+			@RequestPart(name = "dataCsvFile") MultipartFile dataCsvFile) throws Exception {
+		File file = autoTestGeneratorService.runPostmanCollection(postmanCollection, dataCsvFile);
+		Path path = Paths.get(file.getAbsolutePath());
+		ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+		HttpHeaders header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+		header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		header.add("Pragma", "no-cache");
+		header.add("Expires", "0");
+
+		return ResponseEntity.ok().headers(header).contentLength(file.length())
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
+	}
+
+	@PostMapping(name = "RunPostmanCollectionWithDataMap", value = "/runPostmanCollectionWithDataMap", consumes = {
+			MediaType.MULTIPART_FORM_DATA_VALUE })
+	public ResponseEntity<Resource> runPostmanCollection(
+			@RequestPart(required = true, name = "postmanCollection") @NotNull MultipartFile postmanCollection,
+			@RequestPart(name = "apiData") HashMap<String, Object> dataMap) throws Exception {
+
+		File file = autoTestGeneratorService.runPostmanCollection(postmanCollection, dataMap);
+		Path path = Paths.get(file.getAbsolutePath());
+		ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+		HttpHeaders header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+		header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		header.add("Pragma", "no-cache");
+		header.add("Expires", "0");
+
+		return ResponseEntity.ok().headers(header).contentLength(file.length())
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
+	}
+
+	@PostMapping(name = "RunPostmanCollectionWithDataMapAndFiles", value = "/runPostmanCollectionWithDataMapAndFiles", consumes = {
+			MediaType.MULTIPART_FORM_DATA_VALUE })
+	public ResponseEntity<Resource> runPostmanCollection(
+
+			@RequestPart(required = true, name = "postmanCollection") @NotNull MultipartFile postmanCollection,
+			@RequestPart(name = "dataMap") @NotNull HashMap<String, Object> dataMap,
+			@RequestPart(name = "dataFiles") @Nullable List<MultipartFile> dataFiles) throws Exception {
+
+		File file;
+		if (dataFiles != null) {
+			if (!dataFiles.isEmpty()) {
+				file = autoTestGeneratorService.runPostmanCollection(postmanCollection, dataMap, dataFiles);
+			} else {
+				file = autoTestGeneratorService.runPostmanCollection(postmanCollection, dataMap);
+			}
+		} else {
+			file = autoTestGeneratorService.runPostmanCollection(postmanCollection, dataMap);
+		}
+
+		Path path = Paths.get(file.getAbsolutePath());
+		ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+		HttpHeaders header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+		header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		header.add("Pragma", "no-cache");
+		header.add("Expires", "0");
+
+		return ResponseEntity.ok().headers(header).contentLength(file.length())
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
+	}
+
+}
